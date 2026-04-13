5ab66a9783bd694d1090df34022f986682434dd6
lol2k4u-byte <lol2k4u@hotmail.com>
2026-04-13 21:29:36 +0200

    Update ticker.js

--- javascripts/ticker.js
+++ javascripts/ticker.js
@@ -1,4 +1,23 @@
+import { createTickerCard } from "./tickerCard.js";
+
 const params = new URLSearchParams(window.location.search);
 const id = params.get("id");
 
-document.getElementById("companyName").textContent = `${id}`;
+const element = {
+	id: 1,
+    navn: "Ericsson B",
+    logo: "E",
+    vaerdi: "22.462 kr",
+    afkast: "+7,71%",
+    idag: "-1,62%",
+    seneste: "109,15 SEK"
+  };
+
+const card = createTickerCard(element);
+document.getElementById("tickerCard").appendChild(card);
+
+document.addEventListener("click", () => {
+  document.querySelectorAll(".popup-menu").forEach(menu => {
+    menu.classList.remove("vis");
+  });
+});
