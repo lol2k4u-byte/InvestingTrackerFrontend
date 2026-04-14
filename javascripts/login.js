967453074d343c750f840ac3b99f58fdf3f02a2b
Johnny Bach Simonsen <lol2k4u@hotmail.com>
2026-04-14 17:27:50 +0200

    login

--- javascripts/login.js
+++ javascripts/login.js
@@ -1,15 +1,28 @@
-function loginUser(event) {
+async function loginUser(event) {
     event.preventDefault();
 
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
     const message = document.getElementById("message");
 
-    if (username === "admin" && password === "1234") {
+      const response = await fetch("/api/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        username,
+        password
+    })
+  });
+
+  const data = await response.json();
+
+  if (data.token) {
+    localStorage.setItem("token", data.textContent);
       message.style.color = "green";
       message.textContent = "Login lykkedes!";
-        localStorage.setItem("token", "min-token");
-    } else {
+  }else {
     message.style.color = "red";
     message.textContent = "Forkert brugernavn eller adgangskode.";
   }
