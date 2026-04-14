eaa04b5a0580fe70eb05afc12996c1e8e9609711
lol2k4u-byte <lol2k4u@hotmail.com>
2026-04-14 14:16:43 +0200

    Update login.js

--- javascripts/login.js
+++ javascripts/login.js
@@ -1,21 +1,18 @@
 function loginUser(event) {
-    alert('1');
     event.preventDefault();
-alert('2');
+
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
     const message = document.getElementById("message");
-alert('3');
+
     if (username === "admin" && password === "1234") {
-        alert('4');
       message.style.color = "green";
       message.textContent = "Login lykkedes!";
     } else {
-        alert('5');
     message.style.color = "red";
     message.textContent = "Forkert brugernavn eller adgangskode.";
   }
-alert('6');
+
   return false;
 }
 
