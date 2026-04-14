9cb9ff3e7f3e7a20e7bfa92840d3eb6d86cbddce
lol2k4u-byte <lol2k4u@hotmail.com>
2026-04-14 17:39:25 +0200

    Update login.js

--- javascripts/login.js
+++ javascripts/login.js
@@ -1,33 +1,44 @@
 async function loginUser(event) {
     event.preventDefault();
 
-    const username = document.getElementById("username").value;
+    const email = document.getElementById("username").value;
     const password = document.getElementById("password").value;
     const message = document.getElementById("message");
 
-      const response = await fetch("/api/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        username,
-        password
-    })
-  });
+    try {
+        const response = await fetch("https://investingtracker.onrender.com/Login/login", {
+            method: "POST",
+            headers: {
+                "accept": "text/plain",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                email: email,
+                password: password
+            })
+        });
 
-  const data = await response.json();
+        const data = await response.text();
 
-  if (data.token) {
-    localStorage.setItem("token", data.textContent);
-      message.style.color = "green";
-      message.textContent = "Login lykkedes!";
-  }else {
-    message.style.color = "red";
-    message.textContent = "Forkert brugernavn eller adgangskode.";
-  }
+        if (response.ok) {
+            localStorage.setItem("token", data);
 
-  return false;
+            message.style.color = "green";
+            message.textContent = "Login lykkedes!";
+
+        } else {
+            message.style.color = "red";
+            message.textContent = "Forkert login.";
+        }
+
+    } catch (error) {
+        console.error(error);
+        message.style.color = "red";
+        message.textContent = "Kan ikke kontakte serveren.";
+    }
 }
 
-document.getElementById("loginForm").addEventListener("submit", loginUser);
+document
+    .getElementById("loginForm")
+    .addEventListener("submit", loginUser);
+
