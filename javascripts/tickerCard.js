19f0a032815aefcfde8a4ca490109a791f8fc8eb
lol2k4u-byte <lol2k4u@hotmail.com>
2026-04-15 12:52:06 +0200

    Update tickerCard.js

--- javascripts/tickerCard.js
+++ javascripts/tickerCard.js
@@ -6,46 +6,46 @@ export function createTickerCard(element) {
   const idagClass = element.idag.startsWith("-") ? "negativeAmount" : "positiveAmount";
 
   card.innerHTML = `
-    <div class="menu-wrapper">
-      <button class="menu-knap">⋯</button>
-      <div class="popup-menu">
-        <div class="menu-item">Handel</div>
-        <div class="menu-item">Option</div>
-        <div class="menu-item">Udbytte</div>
+    <div class="cardItemMenu-wrapper">
+      <button class="cardItemMenu-button">⋯</button>
+      <div class="cardItemPopup-menu">
+        <div class="cardItemMenu-item">Handel</div>
+        <div class="cardItemMenu-item">Option</div>
+        <div class="cardItemMenu-item">Udbytte</div>
       </div>
     </div>
 
-    <div class="item-header">
-      <div class="logo">${element.logo}</div>
-      <div class="navn">${element.navn}</div>
+    <div class="cardItemHeader">
+      <div class="cardItemLogo">${element.logo}</div>
+      <div class="cardItemStatName">${element.navn}</div>
     </div>
 
-    <div class="stats">
+    <div class="cardItemStats">
       <div class="stat">
-        <div class="label">Værdi</div>
-        <div class="value">${element.vaerdi}</div>
+        <div class="cardItemLabel">Værdi</div>
+        <div class="cardItemValue">${element.vaerdi}</div>
       </div>
 
-      <div class="stat">
-        <div class="label">Afkast</div>
-        <div class="value ${afkastClass}">${element.afkast}</div>
+      <div class="cardItemStat">
+        <div class="cardItemLabel">Afkast</div>
+        <div class="cardItemValue ${afkastClass}">${element.afkast}</div>
       </div>
 
-      <div class="stat">
-        <div class="label">I dag</div>
-        <div class="value ${idagClass}">${element.idag}</div>
+      <div class="cardItemStat">
+        <div class="cardItemLabel">I dag</div>
+        <div class="cardItemValue ${idagClass}">${element.idag}</div>
       </div>
 
-      <div class="stat">
-        <div class="label">Seneste</div>
-        <div class="value">${element.seneste}</div>
+      <div class="cardItemStat">
+        <div class="cardItemLabel">Seneste</div>
+        <div class="cardItemValue">${element.seneste}</div>
       </div>
     </div>
   `;
 
-  const menuKnap = card.querySelector(".menu-knap");
-  const popupMenu = card.querySelector(".popup-menu");
-  const menuItems = card.querySelectorAll(".menu-item");
+  const menuKnap = card.querySelector(".cardItemMenu-button");
+  const popupMenu = card.querySelector(".cardItemPopup-menu");
+  const menuItems = card.querySelectorAll(".cardItemMenu-item");
 
   card.addEventListener("click", () => {
     window.location.href = `ticker.html?id=${element.id}`;
@@ -53,7 +53,7 @@ export function createTickerCard(element) {
 
   menuKnap.addEventListener("click", (e) => {
     e.stopPropagation();
-    popupMenu.classList.toggle("vis");
+    popupMenu.classList.toggle("show");
   });
 
   popupMenu.addEventListener("click", (e) => {
